524b78cc3a5e5d07126f1fa13cbc994a65f16f08
Jair1811 <jairmla018@gmail.com>
2026-05-23 00:01:55 -0500

    feat: agrego IRoleManager para gestion de roles

---
+++ BarberiaReservas.Application/Interfaces/IRoleManager.cs
@@ -0,0 +1,8 @@
+﻿namespace BarberiaReservas.Application.Interfaces;
+
+public interface IRoleManager
+{
+    bool IsValidRole(string role);
+    Task<bool> PromoteToAdminAsync(int userId);
+    Task<bool> DemoteToClientAsync(int userId);
+}
