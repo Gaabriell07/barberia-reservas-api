986f83791e6e3cab927eba1221fd451e30a742fa
YairGabriel <yairperedaalvarado@gmail.com>
2026-05-23 21:33:18 -0500

    feat: implementar AuthService

--- BarberiaReservas.Application/Interfaces/IUserService.cs
+++ BarberiaReservas.Application/Interfaces/IUserService.cs
@@ -8,4 +8,7 @@ public interface IUserService
 {
     Task<User?> GetUserByIdAsync(int id);
     Task<IEnumerable<User>> GetAllUsersAsync();
+    // Métodos añadidos para que el módulo de Auth pueda delegar la persistencia respetando SRP
+    Task<User?> GetUserByEmailAsync(string email);
+    Task CreateUserAsync(User user);
 }
